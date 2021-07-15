9b6ec898bf61929e47f04a86c7abcddb1b38e656
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-07-15 23:13:53 +0200

    Update Folder-Permission-Groups-Tool.ps1

--- Folder-Permission-Groups-Tool.ps1
+++ Folder-Permission-Groups-Tool.ps1
@@ -26,90 +26,36 @@ if (-not ([adsi]::Exists("LDAP://$RootOU"))) {
 $OrganisationalUnits = [ordered]@{}
 Get-ADOrganizationalUnit -SearchBase $RootOU -SearchScope Subtree -Filter * | ForEach-Object {
     $OrganisationalUnits.Add($_.Name, $_.DistinguishedName)
-    }
+}
 
 $LogTitle = "********************************************************  Folder Permission Groups Tool Log  *********************************************************"
 $LogSeparator = "******************************************************************************************************************************************************"
     
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
-<#
-.SYNOPSIS
-Writes a log entry to console, log file and report file.
-
-.DESCRIPTION
-Creates a log entry with timestamp and message passed thru a parameter Message, and saves the log entry to log file, to report log
-file, and writes the same entry to console. In "Settings.cfg" file paths to report log and permanent log file are contained, and
-option to turn on or off whether a console output, report log and permanent log should be written. If "Settings.cfg" file is absent
-it loads the default values. Depending on the NoTimestamp parameter, log entry can be written with or without a timestamp.
-Format of the timestamp is "yyyy.MM.dd. HH:mm:ss:fff", and this function adds " - " after timestamp and before the main message.
-
-.PARAMETER Message
-A string message to be written as a log entry
-
-.PARAMETER NoTimestamp
-A switch parameter if present timestamp is disabled in log entry
-
-.EXAMPLE
-Write-Log -Message "A log entry"
-
-.EXAMPLE
-Write-Log "A log entry"
-
-.EXAMPLE
-Write-Log -Message "===========" -NoTimestamp
-
-.EXAMPLE
-"A log entry" | Write-Log
-
-.NOTES
-Version:        2.2
-Author:         Zoran Jankov
-#>
 function Write-Log {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true,
-                   Position = 0,
-                   ValueFromPipeline = $true,
-                   ValueFromPipelineByPropertyName = $true,
-                   HelpMessage = "A string message to be written as a log entry")]
+            Position = 0,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "A string message to be written as a log entry")]
         [string]
         $Message,
 
         [Parameter(Mandatory = $false,
-                   Position = 1,
-                   ValueFromPipeline = $true,
-                   ValueFromPipelineByPropertyName = $true,
-                   HelpMessage = "A switch parameter if present timestamp is disabled in log entry")]
+            Position = 1,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "A switch parameter if present timestamp is disabled in log entry")]
         [switch]
         $NoTimestamp = $false
     )
 
     begin {
-        if (Test-Path -Path ".\Settings.cfg") {
-            $Settings = Get-Content ".\Settings.cfg" | ConvertFrom-StringData
-
-            $LogFile         = $Settings.LogFile
-            $ReportFile      = $Settings.ReportFile
-            $WriteTranscript = $Settings.WriteTranscript -eq "true"
-            $WriteLog        = $Settings.WriteLog -eq "true"
-            $SendReport      = $Settings.SendReport -eq "true"
-        }
-        else {
-            $Desktop = [Environment]::GetFolderPath("Desktop")
-            $LogFile         = "$Desktop\Log.log"
-            $ReportFile      = "$Desktop\Report.log"
-            $WriteTranscript = $true
-            $WriteLog        = $true
-            $SendReport      = $false
-        }
-        if (-not (Test-Path -Path $LogFile)) {
-            New-Item -Path $LogFile -ItemType File
-        }
-        if ((-not (Test-Path -Path $ReportFile)) -and $SendReport) {
-            New-Item -Path $ReportFile -ItemType File
-        }
+        $Desktop = [Environment]::GetFolderPath("Desktop")
+        $LogFile = "$Desktop\Log.log"
     }
 
     process {
@@ -120,46 +66,18 @@ function Write-Log {
         else {
             $LogEntry = $Message
         }
-
-        if ($WriteTranscript) {
-            Write-Verbose $LogEntry -Verbose
-        }
-        if ($WriteLog) {
-            Add-content -Path $LogFile -Value $LogEntry
-        }
-        if ($SendReport) {
-            Add-content -Path $ReportFile -Value $LogEntry
-        }
+        Add-content -Path $LogFile -Value $LogEntry
     }
 }
 
-<#
-.SYNOPSIS
-
-.DESCRIPTION
-Long description
-
-.PARAMETER InitialDirectory
-Initial directory to be opend with folder browser dialog
-
-.EXAMPLE
-Get-Folder -InitialDirectory "D:\"
-
-.EXAMPLE
-Get-Folder "D:\"
-
-.NOTES
-Version:        1.0
-Author:         Zoran Jankov
-#>
 function Get-Folder {
     [CmdletBinding()]
     param (
-    [Parameter(Mandatory = $true,
-                   Position = 0,
-                   ValueFromPipeline = $true,
-                   ValueFromPipelineByPropertyName = $true,
-                   HelpMessage = "Initial directory to be opend with folder browser dialog")]
+        [Parameter(Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Initial directory to be opend with folder browser dialog")]
         [string]
         $InitialDirectory
     )
@@ -175,49 +93,28 @@ function Get-Folder {
     return $FolderBrowserDialog.SelectedPath
 }
 
-<#
-.SYNOPSIS
-Creates file permissions AD groups for a shared folder
-
-.DESCRIPTION
-Creates file permissions Read Only and Read-Write AD groups for a shared folder, and grants them appropriate access to the shared folder.
-
-.PARAMETER OUPath
-Organization unit path for the permission groups
-
-.PARAMETER FolderPath
-Full path of the shared folder
-
-.EXAMPLE
-New-FilePermissionGroups -OUPath "OU=File Server Permission Groups,DC=company,DC=com" -FolderPath "\\SERVER\Shared_Folder"
-
-.NOTES
-Version:        1.3
-Author:         Zoran Jankov
-#>
 function New-FilePermissionGroups {
     [CmdletBinding(SupportsShouldProcess = $true)]
     param (
         [Parameter(Mandatory = $true,
-                   Position = 0,
-                   ValueFromPipeline = $false,
-                   ValueFromPipelineByPropertyName = $true,
-                   HelpMessage = "Organization unit path for the permission groups")]
+            Position = 0,
+            ValueFromPipeline = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Organization unit path for the permission groups")]
         [string]
         $OUPath,
 
         [Parameter(Mandatory = $true,
-                   Position = 1,
-                   ValueFromPipeline = $false,
-                   ValueFromPipelineByPropertyName = $true,
-                   HelpMessage = "Full path of the shared folder")]
+            Position = 1,
+            ValueFromPipeline = $false,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = "Full path of the shared folder")]
         [string]
         $FolderPath
     )
 
     process {
         $Result = ""
-
         if (-not (Test-Path -Path $FolderPath)) {
             $Message = "ERROR - '$FolderPath' folder does not exists"
             Write-Log -Message $Message
@@ -247,11 +144,11 @@ function New-FilePermissionGroups {
             $Name = $Group.Prefix + $BaseName
             try {
                 New-ADGroup -Name $Name `
-                            -DisplayName $Name `
-                            -Path $OUPath `
-							-GroupCategory Security `
-							-GroupScope Global `
-                            -Description $FolderPath
+                    -DisplayName $Name `
+                    -Path $OUPath `
+                    -GroupCategory Security `
+                    -GroupScope DomainLocal `
+                    -Description $FolderPath
             }
             catch {
                 $Message = "Failed to create '$Name' AD group `r`n" + $_.Exception
@@ -288,99 +185,100 @@ function New-FilePermissionGroups {
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
-$MainForm                        = New-Object system.Windows.Forms.Form
-$MainForm.ClientSize             = New-Object System.Drawing.Point(800,560)
-$MainForm.text                   = "Folder Permission Groups Tool"
-$MainForm.TopMost                = $true
-$MainForm.FormBorderStyle        = 'Fixed3D'
-$MainForm.MaximizeBox            = $false
-$MainForm.ShowIcon               = $false
+$MainForm = New-Object system.Windows.Forms.Form
+$MainForm.ClientSize = New-Object System.Drawing.Point(800, 560)
+$MainForm.Text = "Folder Permission Groups Tool"
+$MainForm.TopMost = $true
+$MainForm.FormBorderStyle = 'Fixed3D'
+$MainForm.MaximizeBox = $false
+$MainForm.ShowIcon = $false
+$MainForm.ControlBox = $false
 
-$FolderPathLabel                 = New-Object system.Windows.Forms.Label
-$FolderPathLabel.text            = "Folder Path"
-$FolderPathLabel.AutoSize        = $true
-$FolderPathLabel.width           = 25
-$FolderPathLabel.height          = 10
-$FolderPathLabel.location        = New-Object System.Drawing.Point(25,35)
-$FolderPathLabel.Font            = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$FolderPathLabel = New-Object system.Windows.Forms.Label
+$FolderPathLabel.Text = "Folder Path"
+$FolderPathLabel.AutoSize = $true
+$FolderPathLabel.Width = 25
+$FolderPathLabel.Height = 10
+$FolderPathLabel.Location = New-Object System.Drawing.Point(25, 35)
+$FolderPathLabel.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 10)
 
-$OUPathLabel                     = New-Object system.Windows.Forms.Label
-$OUPathLabel.text                = "OU Path"
-$OUPathLabel.AutoSize            = $true
-$OUPathLabel.width               = 25
-$OUPathLabel.height              = 10
-$OUPathLabel.location            = New-Object System.Drawing.Point(25,80)
-$OUPathLabel.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$OUPathLabel = New-Object system.Windows.Forms.Label
+$OUPathLabel.Text = "OU Path"
+$OUPathLabel.AutoSize = $true
+$OUPathLabel.Width = 25
+$OUPathLabel.Height = 10
+$OUPathLabel.Location = New-Object System.Drawing.Point(25, 80)
+$OUPathLabel.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 10)
 
-$FolderPathTextBox               = New-Object system.Windows.Forms.TextBox
-$FolderPathTextBox.multiline     = $false
-$FolderPathTextBox.width         = 530
-$FolderPathTextBox.height        = 20
-$FolderPathTextBox.location      = New-Object System.Drawing.Point(120,30)
-$FolderPathTextBox.Font          = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$FolderPathTextBox = New-Object system.Windows.Forms.TextBox
+$FolderPathTextBox.Multiline = $false
+$FolderPathTextBox.Width = 530
+$FolderPathTextBox.Height = 20
+$FolderPathTextBox.Location = New-Object System.Drawing.Point(120, 30)
+$FolderPathTextBox.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 10)
 
-$CreateGroupsButton              = New-Object system.Windows.Forms.Button
-$CreateGroupsButton.text         = "Create Groups"
-$CreateGroupsButton.width        = 140
-$CreateGroupsButton.height       = 30
-$CreateGroupsButton.location     = New-Object System.Drawing.Point(330,505)
-$CreateGroupsButton.Font         = New-Object System.Drawing.Font('Microsoft Sans Serif',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+$CreateGroupsButton = New-Object system.Windows.Forms.Button
+$CreateGroupsButton.Text = "Create Groups"
+$CreateGroupsButton.Width = 140
+$CreateGroupsButton.Height = 30
+$CreateGroupsButton.Location = New-Object System.Drawing.Point(330, 505)
+$CreateGroupsButton.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 10, [System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
 
-$ResultTextBox                   = New-Object system.Windows.Forms.TextBox
-$ResultTextBox.multiline         = $true
-$ResultTextBox.text              = "Waiting for operation execution..."
-$ResultTextBox.width             = 745
-$ResultTextBox.height            = 360
-$ResultTextBox.ScrollBars        = "Vertical"
-$ResultTextBox.location          = New-Object System.Drawing.Point(25,120)
-$ResultTextBox.Font              = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
-$ResultTextBox.ReadOnly          = $true
-$ResultTextBox.ForeColor         = [System.Drawing.ColorTranslator]::FromHtml("#00FF41")
-$ResultTextBox.BackColor         = [System.Drawing.ColorTranslator]::FromHtml("#0D0208")
+$ResultTextBox = New-Object system.Windows.Forms.TextBox
+$ResultTextBox.Multiline = $true
+$ResultTextBox.Text = "Waiting for operation execution..."
+$ResultTextBox.Width = 745
+$ResultTextBox.Height = 360
+$ResultTextBox.ScrollBars = "Vertical"
+$ResultTextBox.Location = New-Object System.Drawing.Point(25, 120)
+$ResultTextBox.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 10)
+$ResultTextBox.ReadOnly = $true
+$ResultTextBox.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#00FF41")
+$ResultTextBox.BackColor = [System.Drawing.ColorTranslator]::FromHtml("#0D0208")
 
-$OUPathComboBox                  = New-Object system.Windows.Forms.ComboBox
-$OUPathComboBox.width            = 650
-$OUPathComboBox.height           = 5
-$OUPathComboBox.location         = New-Object System.Drawing.Point(120,75)
-$OUPathComboBox.Font             = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
-$OUPathComboBox.AutoCompleteMode   = 'SuggestAppend'
+$OUPathComboBox = New-Object system.Windows.Forms.ComboBox
+$OUPathComboBox.Width = 650
+$OUPathComboBox.Height = 5
+$OUPathComboBox.Location = New-Object System.Drawing.Point(120, 75)
+$OUPathComboBox.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 10)
+$OUPathComboBox.AutoCompleteMode = 'SuggestAppend'
 $OUPathComboBox.AutoCompleteSource = 'ListItems'
 
-$SelectFolderButton              = New-Object system.Windows.Forms.Button
-$SelectFolderButton.text         = "Select Folder"
-$SelectFolderButton.width        = 100
-$SelectFolderButton.height       = 30
-$SelectFolderButton.location     = New-Object System.Drawing.Point(672,25)
-$SelectFolderButton.Font         = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$SelectFolderButton = New-Object system.Windows.Forms.Button
+$SelectFolderButton.Text = "Select Folder"
+$SelectFolderButton.Width = 100
+$SelectFolderButton.Height = 30
+$SelectFolderButton.Location = New-Object System.Drawing.Point(672, 25)
+$SelectFolderButton.Font = New-Object System.Drawing.Font('Microsoft Sans Serif', 10)
 
 $MainForm.controls.AddRange(@(
-    $FolderPathLabel,
-    $OUPathLabel,
-    $FolderPathTextBox,
-    $CreateGroupsButton,
-    $ResultTextBox,
-    $OUPathComboBox,
-    $SelectFolderButton
-))
+        $FolderPathLabel,
+        $OUPathLabel,
+        $FolderPathTextBox,
+        $CreateGroupsButton,
+        $ResultTextBox,
+        $OUPathComboBox,
+        $SelectFolderButton
+    ))
 
-$SelectFolderButton.Add_Click({
-    $FolderPathTextBox.text = Get-Folder -InitialDirectory "D:\"
-})
+$SelectFolderButton.Add_Click( {
+        $FolderPathTextBox.Text = Get-Folder -InitialDirectory "D:\"
+    })
 
-$FolderPathTextBox.Add_Click({
-    $FolderPathTextBox.text = Get-Folder -InitialDirectory "D:\"
-})
+$FolderPathTextBox.Add_Click( {
+        $FolderPathTextBox.Text = Get-Folder -InitialDirectory "D:\"
+    })
 
-$CreateGroupsButton.Add_Click({
-    Write-Log -Message $LogTitle -NoTimestamp
-    Write-Log -Message $LogSeparator -NoTimestamp
-    $OUPath = $OrganisationalUnits.Get_Item($OUPathComboBox.Text)
-    New-FilePermissionGroups -OUPath $OUPath -FolderPath $FolderPathTextBox.text - $ |
-    ForEach-Object {
-        $ResultTextBox.Text = $_
-    }
-    Write-Log -Message $LogSeparator -NoTimestamp
-})
+$CreateGroupsButton.Add_Click( {
+        Write-Log -Message $LogTitle -NoTimestamp
+        Write-Log -Message $LogSeparator -NoTimestamp
+        $OUPath = $OrganisationalUnits.Get_Item($OUPathComboBox.Text)
+        New-FilePermissionGroups -OUPath $OUPath -FolderPath $FolderPathTextBox.text - $ |
+        ForEach-Object {
+            $ResultTextBox.Text = $_
+        }
+        Write-Log -Message $LogSeparator -NoTimestamp
+    })
 
 foreach ($Item in $OrganisationalUnits.Keys.GetEnumerator()) {
     $OUPathComboBox.Items.Add($Item)
